916739f8f66b1fa7124b93a58886d253f33b2768
Torsten Grote <t@grobox.de>
2017-12-08 13:31:38 -0200

    Remove Briar Unofficial and update Transportr
    Also remove signing key hack

--- update.sh
+++ update.sh
@@ -1,3 +1,1 @@
-sed -i 's@^signing@#signing@' ${HOME}/.gradle/gradle.properties
 fdroid build --all -v && fdroid publish -v && fdroid update -v && fdroid server update -v
-sed -i 's@^#signing@signing@' ${HOME}/.gradle/gradle.properties
